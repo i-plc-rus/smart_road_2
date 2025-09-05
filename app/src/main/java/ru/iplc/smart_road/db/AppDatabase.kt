253c7d4adc9ef9692713f95670fd8f5b7dd59523
shalimov <shalimov@profiterole.group>
2025-09-05 13:35:33 +0300

    fix new db

--- app/src/main/java/ru/iplc/smart_road/db/AppDatabase.kt
+++ app/src/main/java/ru/iplc/smart_road/db/AppDatabase.kt
@@ -24,7 +24,7 @@ abstract class AppDatabase : RoomDatabase() {
                 val instance = Room.databaseBuilder(
                     context.applicationContext,
                     AppDatabase::class.java,
-                    "pothole_db"
+                    "pothole_db_v2"
                 ).build()
                 INSTANCE = instance
                 instance
